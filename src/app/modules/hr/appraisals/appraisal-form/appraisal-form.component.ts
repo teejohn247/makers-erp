b1042b26964b7426b026e527ae3c8a95df24eb5b
aghosby <etiboaghogho@gmail.com>
2026-02-10 17:42:17 +0000

    Change kpi id

--- src/app/modules/hr/appraisals/appraisal-form/appraisal-form.component.ts
+++ src/app/modules/hr/appraisals/appraisal-form/appraisal-form.component.ts
@@ -460,7 +460,7 @@ export class AppraisalFormComponent implements OnInit {
         kpiGroups: this.generateKpiGrpValues()
       }
       console.log(data);
-      this.hrService.submitAppraisalReview(data, this.periodInView.employeeId).subscribe({
+      this.hrService.submitAppraisalReview(data, this.periodInView.employeeKpiId).subscribe({
         next: res => {
           console.log(res);
           if(res.status == 200) {
