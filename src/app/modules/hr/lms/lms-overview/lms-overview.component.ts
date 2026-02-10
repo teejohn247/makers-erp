0f2cba860a78f0b352928d37a9ea9405192f4e8a
aghosby <etiboaghogho@gmail.com>
2026-02-10 17:41:06 +0000

    Minor changes

--- src/app/modules/hr/lms/lms-overview/lms-overview.component.ts
+++ src/app/modules/hr/lms/lms-overview/lms-overview.component.ts
@@ -40,63 +40,63 @@ export class LmsOverviewComponent implements OnInit {
       console.log(this.courses)
     })
 
-    this.courseCategories = [
-      {
-        name: 'Compliance',
-        noOfCourses: 7,
-        totalTime: '2hrs 30mins'
-      },
-      {
-        name: 'Leadership',
-        noOfCourses: 2,
-        totalTime: '1hr 20mins'
-      },
-      {
-        name: 'Management',
-        noOfCourses: 5,
-        totalTime: '3hrs'
-      },
-      {
-        name: 'Health & Safety',
-        noOfCourses: 10,
-        totalTime: '7hrs'
-      }
-    ]
+    // this.courseCategories = [
+    //   {
+    //     name: 'Compliance',
+    //     noOfCourses: 7,
+    //     totalTime: '2hrs 30mins'
+    //   },
+    //   {
+    //     name: 'Leadership',
+    //     noOfCourses: 2,
+    //     totalTime: '1hr 20mins'
+    //   },
+    //   {
+    //     name: 'Management',
+    //     noOfCourses: 5,
+    //     totalTime: '3hrs'
+    //   },
+    //   {
+    //     name: 'Health & Safety',
+    //     noOfCourses: 10,
+    //     totalTime: '7hrs'
+    //   }
+    // ]
 
-    this.courses = [
-      {
-        name: 'Project Management PIMP & ACAP',
-        category: 'Management',
-        thumbnail: 'https://greenpeg.com/wp-content/uploads/2023/03/workers.jpeg',
-        noOfParticipants: 10,
-        duration: '2hr 10mins',
-        rating: 4.5
-      },
-      {
-        name: 'HSE Compliance',
-        category: 'Compliance',
-        thumbnail: 'https://bgsnigerialimited.com/wp-content/uploads/2024/03/equipment-Lease.jpg',
-        noOfParticipants: 20,
-        duration: '1hr 15mins',
-        rating: 3.5
-      },
-      {
-        name: 'Sexual harrasment in the workplace',
-        category: 'Compliance',
-        thumbnail: 'assets/images/illustrations/sexual-harrasment.jpg',
-        noOfParticipants: 6,
-        duration: '1hr',
-        rating: 4.8
-      },
-      {
-        name: 'Managing surbodinates effectively',
-        category: 'Leadership',
-        thumbnail: 'assets/images/illustrations/leadership.png',
-        noOfParticipants: 6,
-        duration: '4hr',
-        rating: 4
-      }
-    ]
+    // this.courses = [
+    //   {
+    //     name: 'Project Management PIMP & ACAP',
+    //     category: 'Management',
+    //     thumbnail: 'https://greenpeg.com/wp-content/uploads/2023/03/workers.jpeg',
+    //     noOfParticipants: 10,
+    //     duration: '2hr 10mins',
+    //     rating: 4.5
+    //   },
+    //   {
+    //     name: 'HSE Compliance',
+    //     category: 'Compliance',
+    //     thumbnail: 'https://bgsnigerialimited.com/wp-content/uploads/2024/03/equipment-Lease.jpg',
+    //     noOfParticipants: 20,
+    //     duration: '1hr 15mins',
+    //     rating: 3.5
+    //   },
+    //   {
+    //     name: 'Sexual harrasment in the workplace',
+    //     category: 'Compliance',
+    //     thumbnail: 'assets/images/illustrations/sexual-harrasment.jpg',
+    //     noOfParticipants: 6,
+    //     duration: '1hr',
+    //     rating: 4.8
+    //   },
+    //   {
+    //     name: 'Managing surbodinates effectively',
+    //     category: 'Leadership',
+    //     thumbnail: 'assets/images/illustrations/leadership.png',
+    //     noOfParticipants: 6,
+    //     duration: '4hr',
+    //     rating: 4
+    //   }
+    // ]
   }
 
   addNewCourse() {
